e26b0ddac41d4b81af38b8ff27936bf6536e3608
Amaka <jomakwere@gmail.com>
2025-02-19 13:37:57 +0100

    Header content and resume re-adjustment 19/2/2025

--- src/components/HeaderContentPage.jsx
+++ src/components/HeaderContentPage.jsx
@@ -4,11 +4,11 @@ const HeaderContentPage = (props) => {
   const { headerText, icon } = props;
 
   return (
-    <div className="lg:flex md:flex md:items-center md:justify-center md:p-[10.5] sm:block  sm:items-center sm:p-12 sm:justify-center  items-center justify-center p-11 border-b border-dashed border-b-yellow-500">
-      <h2 className="leading-[1rem] mt-10 text-[36px] sm:mb-10 font-bold tracking-[1.5px] uppercase text-yellow-500 flex flex-grow">
+    <div className="flex flex-col md:flex-row md:items-center md:justify-center md:p-[10.5] sm:block  sm:items-center sm:p-12 sm:justify-center  items-center justify-center p-11 border-b border-dashed border-b-yellow-500">
+      <h2 className="leading-normal mt-10 text-[36px] sm:mb-10 font-bold tracking-wide uppercase text-yellow-500 flex flex-grow">
         {headerText}
       </h2>
-      <span className="lg:mt-10 mt-20">{icon}</span>
+      <span className="mt-4 md:mt-0 md:ml-4">{icon}</span>
     </div>
   );
 };
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import HeaderContentPage from "./HeaderContentPage";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-import 'react-vertical-timeline-component/style.min.css'
-import {MdWork} from "react-icons/md";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdWork } from "react-icons/md";
 
 const Data = {
   experience: [
+    {
+      title: "Independent Consultant",
+      subTitle: "jomak enviromental services",
+      description:
+        "Involved in Environment impact assesement, Material testing, environmental consultancy, Crisis Management.",
+      date: "2022 till date",
+    },
     {
       title: "Vice princpal",
       subTitle: "Universal Comprehensive colledge",
       description:
         "Involved in Administrative Leadership, Curriculum Developnment, Staff Support, Students Engagement, Crisis Mangement and Data Analysis.",
-        date: "2013-2022"
+      date: "2013-2022",
     },
 
     {
@@ -20,7 +30,7 @@ const Data = {
       subTitle: "LandMark Colledge",
       description:
         "Lesson Planning, Instruction Delivery, Assessment, Student Support, Laboratory management, Fostering Interest, Proffessional Development",
-        date: "2009-2012"
+      date: "2009-2012",
     },
 
     {
@@ -28,7 +38,7 @@ const Data = {
       subTitle: "LandMark Colledge",
       description:
         "Lesson Planning, Instruction Delivery, Assessment, Student Support, Laboratory management, Fostering Interest, Proffessional Development",
-        date: "2008-2009"
+      date: "2008-2009",
     },
 
     {
@@ -36,7 +46,7 @@ const Data = {
       subTitle: "Global Trust Marine",
       description:
         "Servicing, Maintaining and Teasting Equipment to ensure they in good state.",
-        date: "2004-20205"
+      date: "2004-20205",
     },
   ],
   education: [
@@ -44,42 +54,42 @@ const Data = {
       title: "Secondary School",
       subTitle: "Secondary technical School, Aba, Abia State, Nigeria ",
       description: "Senior Secondary Certificate of Education",
-      date: "1992-1998"
+      date: "1992-1998",
     },
 
     {
       title: "National Diploma",
       subTitle: " Federal University of Technology Owerri",
       description: "Science Laboratory Technology",
-      date: "2003-2005"
+      date: "2003-2005",
     },
 
     {
       title: " Higher National Diploma",
       subTitle: " Imo State Polytechinc, Owerri",
       description: "Microbiology",
-      date: "2005-2007"
+      date: "2005-2007",
     },
 
     {
       title: "Post Graduate Diploma",
       subTitle: " Lagos state University, Ojo.",
       description: "Industrial Biochemistry",
-      date: "2015-2017"
+      date: "2015-2017",
     },
 
     {
       title: "Masters in Environmental Management",
       subTitle: " Lagos state University, Ojo.",
       description: "Environmental Management",
-      date: "2019-2022"
+      date: "2019-2022",
     },
 
     {
       title: "Full stack Develoment Certificate",
       subTitle: "African Institute for Technology and Innovation.",
       description: "Full Stack Developer",
-      date: "2023-2024"
+      date: "2023-2024",
     },
   ],
 };
@@ -92,65 +102,56 @@ const Resume = () => {
         icon={<BsInfoCircleFill size={40} />}
       />
       <div className="lg:flex block w-[100%] ">
-         
-       <div className="w-[50%] flex flex-col gap-[2rem] flex-1 ">
-          <h1 className="inline-block text-[#181818] text-[22px] font-bold leading-[1rem] tracking-[0.5px] p-[20px] self-center border-b border-solid border-yellow-500  relative uppercase " >Experience</h1>
-          <VerticalTimeline
-          layout={"1-column"}
-          lineColor="#ffdd40"
-          >
-            {Data.experience.map((item,i)=>(
+        <div className="w-[50%] flex flex-col gap-[2rem] flex-1 ">
+          <h1 className="inline-block text-[#181818] text-[22px] font-bold leading-[1rem] tracking-[0.5px] p-[20px] self-center border-b border-solid border-yellow-500  relative uppercase ">
+            Experience
+          </h1>
+          <VerticalTimeline layout={"1-column"} lineColor="#ffdd40">
+            {Data.experience.map((item, i) => (
               <VerticalTimelineElement
-              key={i}
-              className=" flex  items-center flex-1"
-              contentStyle={{
-                background:"none",
-                color: "#fffff",
-                border: "1.5px solid #fffff"
-              }}
-
-              icon = {<MdWork/>}
-              iconStyle={{
-                background: "#181818",
-                color:"yellow"
-              }}
+                key={i}
+                className=" flex  items-center flex-1"
+                contentStyle={{
+                  background: "none",
+                  color: "#fffff",
+                  border: "1.5px solid #fffff",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "yellow",
+                }}
               >
-                <div >
+                <div>
                   <h2 className="font-bold">{item.title}</h2>
                   <h3>{item.subTitle}</h3>
                   <h4>{item.description}</h4>
                 </div>
-                <p className="text-[18px]"                                                                                                                                                                                                                                                                                                                                                                                                  >{item.date}</p>
-
+                <p className="text-[18px]">{item.date}</p>
               </VerticalTimelineElement>
             ))}
-
           </VerticalTimeline>
         </div>
 
         <div className="w-[50%] flex flex-col gap-[2rem]">
-          <h1 className="inline-block text-[#181818] text-[22px] font-bold leading-[1rem] tracking-[0.5px] p-[20px] self-center border-b border-solid border-yellow-500  relative uppercase ">Education</h1>
-          <VerticalTimeline
-          layout={"1-column"}
-          lineColor="#ffdd40"
-          >
-
-{Data.education.map((item,i)=>(
+          <h1 className="inline-block text-[#181818] text-[22px] font-bold leading-[1rem] tracking-[0.5px] p-[20px] self-center border-b border-solid border-yellow-500  relative uppercase ">
+            Education
+          </h1>
+          <VerticalTimeline layout={"1-column"} lineColor="#ffdd40">
+            {Data.education.map((item, i) => (
               <VerticalTimelineElement
-              key={i}
-              className="flex items-center last-child:pb-[0px] flex-1"
-              contentStyle={{
-                background:"none",
-                color: "#fffff",
-                border: "1.5px solid #fffff"
-              }}
-              icon = {<MdWork/>}
-              iconStyle={{
-                background: "#181818",
-                color:"yellow"
-              }}
-
-
+                key={i}
+                className="flex items-center last-child:pb-[0px] flex-1"
+                contentStyle={{
+                  background: "none",
+                  color: "#fffff",
+                  border: "1.5px solid #fffff",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "yellow",
+                }}
               >
                 <div className="text-[16px]">
                   <h2 className="font-bold ">{item.title}</h2>
@@ -158,17 +159,10 @@ const Resume = () => {
                   <h4>{item.description}</h4>
                 </div>
                 <p className="text-[18px]">{item.date}</p>
-
               </VerticalTimelineElement>
             ))}
-
           </VerticalTimeline>
-
         </div>
-
-
-
-
       </div>
     </section>
   );
